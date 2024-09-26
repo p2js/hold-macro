88c7fb8b2bc37b6dbcb37b5ad163ed12b07a6fd3
Alfio <tomarchioalfio5@gmail.com>
2024-09-26 17:00:09 +0100

    Add general documentation

---
+++ readme.md
@@ -0,0 +1,5 @@
+# hold-macro
+
+This small library crate provides an alternative `todo!()` macro, with the additional functionality of being able to suppress any errors relating to unused values.
+
+See the source code docs for usage information.
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,43 @@
+#![crate_name = "hold_macro"]
 #![no_std]
 
 #[macro_export]
+/// Macro to use during development to mark something as todo, and suppress unused value warnings by using ("Holding") the values.
+///
+/// # Usage
+///
+/// The macro can be used identically to rust's `todo!()`, either with or without a formatted message:
+/// ```
+/// use hold_macro::hold;
+///
+/// fn example_function_1() {
+///     hold!();
+/// }
+///
+/// fn example_function_2() {
+///     hold!("must implement {} more features", 5);
+/// }
+/// ```
+///
+/// However, if you have some placeholder variables (eg. to make a function signature) that you aren't using yet, you can "hold" those values to suppress any warnings:
+/// ```
+/// fn multiply(x: i32, y: i32) {
+///     hold!(x, y); // No warning !!
+/// }
+/// ```
+/// The macro will accept any identifier.
+///
+/// If you want to both hold values and leave a message, separate the two with a semicolon:
+/// ```
+/// fn add(x: i32, y: i32) {
+///     hold!(x, y; "Don't know addition yet");
+/// }
+///
+/// fn subtract(x: i32, y: i32) {
+///     hold!(x, y; "I'm reading a book to learn arithmetic: {} chapter(s) completed", 0);
+/// }
+/// ```
+///
 macro_rules! hold {
     ($($vars:ident),+ $(; $($msg:tt)+)?) => {{
         let __unused = ($($vars),*);
